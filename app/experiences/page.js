17b97ae296e4e77f1cc0b98860f5e69cb4686aff
ppmmhh <pia.hoelzl@gmx.at>
2024-03-21 14:27:25 +0100

    Create and setup file

---
+++ app/experiences/page.js
@@ -0,0 +1,62 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { getExperiencesInsecure } from '../../database/experiences';
+import styles from './page.module.scss';
+
+export const metadata = {
+  title: 'Experience page',
+  description: 'This page shows all the upcoming experiences',
+};
+
+export default async function ExperiencessPage() {
+  const experiences = await getExperiencesInsecure();
+
+  return (
+    <div className={styles.sectionContainer}>
+      <div>
+        <h1>Upcoming Experiences</h1>
+      </div>
+      <div>
+        <div className={styles.expContainer}>
+          {experiences.map((experience) => {
+            return (
+              <div key={`experiences-${experience.id}`}>
+                <Link
+                  href={`/experiences/${experience.id}`}
+                  data-test-id={`experience-${experience.id}`}
+                  className={styles.experienceItem}
+                >
+                  <Image
+                    src={experience.image}
+                    width={250}
+                    height={300}
+                    alt={experience.title}
+                    className={styles.expImage}
+                  />
+                  <div className={styles.expDetails}>
+                    <div className={styles.headline}>
+                      <h2>{experience.title}</h2>
+                    </div>
+                    <div>Date: {experience.workshop_date}</div>
+                    <div>Time: {experience.timeframe}</div>
+                    <br />
+                    <div>
+                      {experience.description.length > 100
+                        ? experience.description.slice(
+                            0,
+                            experience.description.lastIndexOf(' ', 300),
+                          )
+                        : experience.description}
+                      {experience.description.length > 300 && ' [...]'}
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
