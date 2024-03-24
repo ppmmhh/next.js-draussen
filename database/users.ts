f3aaa240142ddce8a26f7d8e8c3aabe67e50f85d
ppmmhh <pia.hoelzl@gmx.at>
2024-03-24 14:40:19 +0100

    Add users to database

---
+++ database/users.ts
@@ -0,0 +1,78 @@
+import { cache } from 'react';
+import { sql } from './connect';
+
+export type User = {
+  id: number;
+  email: string;
+  username: string;
+};
+
+export type UserWithPasswordHash = User & {
+  passwordHash: string;
+};
+
+export const getUser = cache(async (token: string) => {
+  const [user] = await sql<Pick<User, 'username'>[]>`
+    SELECT
+      users.username
+    FROM
+      users
+      INNER JOIN sessions ON (
+        sessions.token = ${token}
+        AND sessions.user_id = users.id
+        AND sessions.expiry_timestamp > now()
+      )
+  `;
+  return user;
+});
+
+export const getUserByUsernameInsecure = cache(async (username: string) => {
+  const [user] = await sql<User[]>`
+    SELECT
+      id,
+      email,
+      username
+    FROM
+      users
+    WHERE
+      username = ${username.toLowerCase()}
+  `;
+  return user;
+});
+
+export const getUserWithPasswordHashByUsernameInsecure = cache(
+  async (username: string) => {
+    const [user] = await sql<UserWithPasswordHash[]>`
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        username = ${username.toLowerCase()}
+    `;
+    return user;
+  },
+);
+
+export const createUserInsecure = cache(
+  async (username: string, email: string, passwordHash: string) => {
+    const [user] = await sql<Pick<User, 'id' | 'username'>[]>`
+      INSERT INTO
+        users (
+          username,
+          email,
+          password_hash
+        )
+      VALUES
+        (
+          ${username.toLowerCase()},
+          ${email},
+          ${passwordHash}
+        )
+      RETURNING
+        id,
+        username
+    `;
+    return user;
+  },
+);
