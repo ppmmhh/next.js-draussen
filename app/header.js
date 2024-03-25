4c7c36a2371cbc5a7a695dd0955aadba3463f84a
ppmmhh <pia.hoelzl@gmx.at>
2024-03-25 20:15:23 +0100

    Add LoginButton to Header

--- app/header.js
+++ app/header.js
@@ -1,18 +1,29 @@
-import './globals.scss';
 import './header.scss';
 import React from 'react';
+import LoginButton from '../app/(auth)/login/LoginButton';
 
 export default function Header() {
   return (
-    <div className="text">
-      draussen is an outdoor community dedicated to a shared natural experience.
-      Together with local experts, we aim to create opportunities for meaningful
-      exchange and learning through guided workshops and recreational activities
-      in nature and beyond.
+    <div className="sectionContainer">
+      <div className="logo">
+        <img src="./images/logo_wo_bg.png" alt="Logo" />
+      </div>
+
+      <div className="text">
+        draussen is an outdoor community dedicated to a shared natural
+        experience. Together we aim to create opportunities for meaningful
+        exchange and learning through guided workshops and recreational
+        activities in nature and beyond.
+      </div>
+
+      <div>
+        <a className="button" href="/login">
+          <LoginButton />
+        </a>
+      </div>
+
       <div className="container">
-        <div className="image-wrapper">
-          <img src="/images/wien1.jpeg" alt="vienna postcard" />
-        </div>
+        <img src="/images/wien1.jpeg" alt="vienna postcard" />
       </div>
     </div>
   );
