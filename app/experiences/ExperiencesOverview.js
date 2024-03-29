640b6a7503ea5d4fdf842d48eb40b2899241863f
ppmmhh <pia.hoelzl@gmx.at>
2024-03-29 13:37:09 +0100

    Adjust filter component and styling

--- app/experiences/ExperiencesOverview.js
+++ app/experiences/ExperiencesOverview.js
@@ -33,7 +33,7 @@ export default async function ExperiencesOverview() {
       <div className={styles.headline}>
         <h1>Upcoming Experiences</h1>
       </div>
-      <div>
+      <div className={styles.expFilter}>
         <ExperiencesFilter />
       </div>
       <div className={styles.expContainer}>
